81882c43094dfcbcc03820b15f9390b8e1643196
James Wilcox <jrw12@cs.washington.edu>
2018-08-08 14:04:34 -0700

    implement ordered set class

--- mypyvy.py
+++ mypyvy.py
@@ -242,6 +242,35 @@ class Diagram(object):
         logging.debug('generalized diag')
         logging.debug(str(self))
 
+class OrderedSet(Generic[T], Iterable[T]):
+    def __init__(self, contents: Optional[Iterable[T]]=None) -> None:
+        self.l: List[T] = []
+        self.s: Set[T] = set()
+
+        if contents is None:
+            contents = []
+
+        for x in contents:
+            self.add(x)
+
+    def __len__(self) -> int:
+        return len(self.l)
+
+    def __str__(self) -> str:
+        return '{%s}' % ','.join(str(x) for x in self.l)
+
+    def add(self, x: T) -> None:
+        if x not in self.s:
+            self.l.append(x)
+            self.s.add(x)
+
+    def remove(self, x: T) -> None:
+        if x not in self.s:
+            raise
+
+    def __iter__(self) -> Iterator[T]:
+        return iter(self.l)
+
 class Model(object):
     def __init__(
             self,
