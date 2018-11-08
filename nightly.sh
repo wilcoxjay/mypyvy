6cae735ea0d75154ef75890bbd7a2f8384959cef
James Wilcox <jrw12@cs.washington.edu>
2018-11-08 10:14:40 -0800

    update nightly script with all combinations of options

--- nightly.sh
+++ nightly.sh
@@ -1,41 +1,45 @@
 #!/usr/bin/env bash
 
-TAG=nightly
-ARGS=
+function cleanup() {
+    DIR="$1"
 
-while [ $# -ne 0 ]; do
-    case "$1" in
-        --args)
-            ARGS="$@"  # include --args
-            while [ $# -ne 0 ]; do
-                shift
-            done
-            break
-            ;;
-        --tag)
-            shift
-            if [ $# -eq 0 ]; then
-                echo "--tag expects an argument"
-                exit 1
-            fi
-            TAG="$1"
-            ;;
-        *)
-            echo "unrecognized argument $1"
-            exit 1
-            ;;
-    esac
-    shift
+    if [ ! -d "$DIR" ]; then
+	echo "not cleaning up non-existant directory $DIR" >&2
+	return 1
+    fi
 
+    pushd $DIR
+    tar cfz logs.tgz *.log
+    rm *.log
+    popd
+
+    git add $DIR
+}
+
+
+LOGDIR=logs/$(date '+%y%m%d-%H%M%S')-updr
+time python3 benchmark.py -n 500 -j 4 --timeout 10 --pin-cores --keep-logs "$LOGDIR"
+cleanup "$LOGDIR"
+
+PHASE_ARGS="-n 50 -j 4 --timeout 120 --pin-cores --benchmark test/lockserv.pyv test/lockserv_multi_view.pyv --keep-logs"
+
+for SIMP in '' --simplify-diagram; do
+    for MIN in '' --minimize-models; do
+	for CORES in '' --use-z3-unsat-core; do
+	    TAG='phase'
+	    if [ "$SIMP"x != x ]; then
+		TAG="${TAG}-simpl"
+	    fi
+	    if [ "$MIN"x != x ]; then
+		TAG="${TAG}-min"
+	    fi
+	    if [ "$CORES"x != x ]; then
+		TAG="${TAG}-cores"
+	    fi
+
+	    LOGDIR=logs/$(date '+%y%m%d-%H%M%S')-$TAG
+	    time python3 benchmark.py $PHASE_ARGS "$LOGDIR" --args --automaton $SIMP $MIN $CORES
+	    cleanup "$LOGDIR"
+	done
+    done
 done
-
-
-LOGDIR=logs/$(date '+%y%m%d-%H%M%S')-$TAG
-time python3 benchmark.py -n 1000 -j 4 --timeout 75 --pin-cores --keep-logs $LOGDIR $ARGS
-
-pushd $LOGDIR
-tar cfz logs.tgz *.log
-rm *.log
-popd
-
-git add $LOGDIR
