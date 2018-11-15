346fa1e66eb8505e89295e5f7fefb3e208eafa4e
James Wilcox <jrw12@cs.washington.edu>
2018-11-14 22:00:10 -0800

    nightly mini-run for cache_one

--- nightly.sh
+++ nightly.sh
@@ -6,7 +6,7 @@ for AUTOMATON in '' --automaton '--automaton --sketch'; do
     else
         TAG=$(echo $AUTOMATON | tr -d '-' | tr ' ' '-' | sed 's/automaton/phase/')
     fi
-    LOGDIR=logs/$(date '+%y%m%d-%H%M%S')-$TAG
+    LOGDIR=logs/$(date '+%y%m%d-%H%M%S')-cache-one-$TAG
     echo "$LOGDIR"
-    time python3 benchmark.py -n 16 -j 16 --all-benchmarks --timeout 1200 --keep-logs "$LOGDIR" --args $AUTOMATON --simplify-diagram --use-z3-unsat-cores --dont-block-may-cex
+    time python3 benchmark.py -n 16 -j 16 --benchmark test/cache_one.pyv --timeout 1200 --keep-logs "$LOGDIR" --args $AUTOMATON --simplify-diagram --use-z3-unsat-cores --dont-block-may-cex
 done
