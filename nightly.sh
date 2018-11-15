107ec875c5726b7eec8fc3bf09558a8e6adb22fc
James Wilcox <jrw12@cs.washington.edu>
2018-11-14 23:27:44 -0800

    oops

--- nightly.sh
+++ nightly.sh
@@ -3,7 +3,7 @@
 
 LOGDIR=logs/$(date '+%y%m%d-%H%M%S')-updr-long-timeout
 echo "$LOGDIR"
-time python3 benchmark.py -n 16 -j 16 --benchmark test/sharded-kv-retransmit.pyv test/cache_one.pyv --timeout 1 --keep-logs "$LOGDIR" --args --simplify-diagram --use-z3-unsat-cores --dont-block-may-cex
+time python3 benchmark.py -n 16 -j 16 --benchmark test/sharded-kv-retransmit.pyv test/cache_one.pyv --timeout 14400 --keep-logs "$LOGDIR" --args --simplify-diagram --use-z3-unsat-cores --dont-block-may-cex
 
 
 # for AUTOMATON in '' --automaton '--automaton --sketch'; do
