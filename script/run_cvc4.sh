a894bef5930fc9439489dc14d7d7b39a5ce130dd
Oded Padon <oded.padon@gmail.com>
2020-07-25 16:48:13 -0700

    support cvc4 version 1.8

--- script/run_cvc4.sh
+++ script/run_cvc4.sh
@@ -5,7 +5,9 @@ if type cvc4 &>/dev/null; then
     CVC4CMD=cvc4
 else
     # from here on, it is linux specific: we wget cvc4 if needed, or use the one in the current dir
-    if type ./cvc4-1.7-x86_64-linux-opt &>/dev/null; then
+    if type ./cvc4-1.8-x86_64-linux-opt &>/dev/null; then
+	CVC4CMD=./cvc4-1.8-x86_64-linux-opt
+    elif type ./cvc4-1.7-x86_64-linux-opt &>/dev/null; then
 	CVC4CMD=./cvc4-1.7-x86_64-linux-opt
     else
 	echo "Error: cannot find CVC4 executable" >&2
