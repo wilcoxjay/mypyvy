cb979b0ae31ec5ac345569f2d17be3edfcfb9926
Abdu Mohamdy <amohamdy@stanford.edu>
2020-08-11 00:23:56 -0700

    added get_relation() in java to use indices and updated KodBoolUnity

--- src/kod.py
+++ src/kod.py
@@ -54,8 +54,8 @@ class KodTranslator:
         return f'{args[0]}{op}({joined_args})'
 
     @staticmethod
-    def relation_to_kod(r: RelationDecl) -> KodExpr:
-        return f'this.relations.get("{r.name}")'
+    def relation_to_kod(r: RelationDecl, index: int) -> KodExpr:
+        return f'this.get_relation("{r.name}", {index})'
 
     @staticmethod
     def var_to_kod(sv: SortedVar) -> KodExpr:
@@ -66,10 +66,10 @@ class KodTranslator:
         return f'this.get_var("{c.name}")'
 
     @staticmethod
-    def boolconst_to_kod(d: RelationDecl, index: int) -> KodExpr:
+    def nullary_to_kod(d: RelationDecl, index: int) -> KodExpr:
         if not d.mutable:
             index = 0
-        return f'this.get_relation("{d.name}", {index}).some()'
+        return f'this.get_relation("{d.name}", {index})'
 
     @staticmethod
     def sort_to_kod(s: Sort) -> KodExpr:
@@ -123,12 +123,12 @@ class KodTranslator:
                 assert False
             elif isinstance(d, FunctionDecl):
                 kod_args = [self._translate_expr(a) for a in e.args]
-                callee = KodTranslator.relation_to_kod(d)
+                callee = KodTranslator.relation_to_kod(d, self.scope.current_state_index)
                 return KodTranslator.join_expr('.join', list(reversed(kod_args)) + [callee])
             elif isinstance(d, RelationDecl):
                 kod_args = [self._translate_expr(a) for a in e.args]
                 product = KodTranslator.join_expr('.product', kod_args)
-                callee = KodTranslator.relation_to_kod(d)
+                callee = KodTranslator.relation_to_kod(d, self.scope.current_state_index)
                 return f'{product}.in({callee})'
             else:
                 assert False, f'{d}\n{e}'
@@ -152,7 +152,8 @@ class KodTranslator:
             d = self.scope.get(e.name)
             assert d is not None, f'{e.name}\n{e}'
             if isinstance(d, RelationDecl): # Nullary relation : BOOL
-                return KodTranslator.boolconst_to_kod(d, self.scope.current_state_index)
+                kod_nullary = KodTranslator.nullary_to_kod(d, self.scope.current_state_index)
+                return f'this.__KodBoolUnity__.intersection({kod_nullary}).some()'
             elif isinstance(d, ConstantDecl):
                 return KodTranslator.const_to_kod(d)
             elif isinstance(d, DefinitionDecl):
@@ -184,6 +185,7 @@ class KodSolver:
         self.class_methods_generators = [ # Order is important here: kod_get_formula must be before kod_get_bounds
           self.kod_get_constructor,
           self.kod_get_get_var,
+          self.kod_get_get_relation,
           self.kod_get_formula,
           self.kod_get_bounds,
           self.kod_get_main,
@@ -195,6 +197,7 @@ class KodSolver:
                 'java.util.Map',
                 'java.util.HashMap',
                 'java.util.Arrays',
+                'java.util.Iterator',
                 'kodkod.ast.Formula',
                 'kodkod.ast.Relation',
                 'kodkod.ast.Expression',
@@ -217,7 +220,20 @@ class KodSolver:
             '  return this.vars.get(name);',
             '}',
         ]
-
+    
+    def kod_get_get_relation(self) -> List[str]:
+        return [
+            'public Relation get_relation(String name, int index) {',
+            '   if (!this.relations.containsKey(name)) {',
+            '      relations.put(name, new HashMap<Integer, Relation>());',
+            '   }',
+            '   if (!this.relations.get(name).containsKey(index)) {',
+            '       int arity = this.arities.get(name).size() == 0? 1: this.arities.get(name).size();',
+            '       this.relations.get(name).put(index, Relation.nary(name, arity));',
+            '   }',
+            '   return this.relations.get(name).get(index);',
+            '}',
+        ]
     def get_translator(self, num_states: int) -> KodTranslator:
         # TODO: support multiple states
         assert num_states == 1
@@ -235,7 +251,7 @@ class KodSolver:
         lines = ["public _KodkodModel() {"]
         lines.extend([
             'this.vars = new HashMap<String, Variable>();',
-            'this.relations = new HashMap<Map.Entry<String, Integer>, Relation>();',
+            'this.relations = new HashMap<String, Map<Integer, Relation>>();',
             'this.arities = new HashMap<String, List<String>>();',
             'this.sorts = new HashMap<String, Relation>();',
         ])
@@ -255,6 +271,7 @@ class KodSolver:
           'public Bounds bounds() {',
         ]
         atoms = [f'"{s.name}{i}"' for s in self.scope.sorts.values() for i in range(self.bound)]
+        atoms.append('\"__KODBOOLUNITY__"')
         atoms_string = ', '.join(atoms)
         lines.extend([
             f'final Universe _univ = new Universe(Arrays.asList({atoms_string}));',
@@ -266,18 +283,22 @@ class KodSolver:
           f'_b.boundExactly(this.sorts.get("{s.name}"), _f.range(_f.tuple("{s.name}0"), _f.tuple("{s.name}{self.bound - 1}")));'
           for s in self.scope.sorts.values()
         )
+        lines.append('_b.boundExactly(this.__KodBoolUnity__, _f.setOf(_f.tuple("__KODBOOLUNITY__")));')
         # Bound relations to their arity
         lines.extend([
-            'for(Relation rel : this.relations.values()) {',
-            '   Iterator<String> it = this.arities.get(rel.name()).iterator();',
-            '   if(!it.hasNext()) { // Nullary relation',
-            '      _b.bound(rel, _f.setOf(_f.tuple("__KodBoolUnity__")));',
-            '   } else {',
-            '      TupleSet up_bounds = _b.upperBound(this.sorts.get(it.next())); ',
-            '      while(it.hasNext()) {',
-            '         up_bounds = up_bounds.product(_b.upperBound(this.sorts.get(it.next())));',
+            'for(Map<Integer, Relation> m : this.relations.values()) {',
+            '   for(Relation rel: m.values()){',
+            '      Iterator<String> it = this.arities.get(rel.name()).iterator();',
+            '      if(!it.hasNext()) { // Nullary relation',
+            '         _b.bound(rel, _f.setOf(_f.tuple("__KODBOOLUNITY__")));',
+            '      } else {',
+            '         TupleSet up_bounds = _b.upperBound(this.sorts.get(it.next())); ',
+            '         while(it.hasNext()) {',
+            '            up_bounds = up_bounds.product(_b.upperBound(this.sorts.get(it.next())));',
+            '         }',
+            '         _b.bound(rel, up_bounds);',
             '      }',
-            '      _b.bound(rel, up_bounds);',
+
             '   }',
             '}',
         ])
@@ -312,7 +333,7 @@ class KodSolver:
         lines: List[str] = [
             'public final class _KodkodModel {',
             'private final Map<String, Variable> vars;',
-            'private final Map<Map.Entry<String, Integer>, Relation> relations;',
+            'private final Map<String, Map<Integer, Relation>> relations;',
             'private final Map<String, List<String>> arities;',
             'private final Map<String, Relation> sorts;',
             'private final Relation __KodBoolUnity__;',
