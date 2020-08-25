8dfc1ffcb299dc8bfea048ffeefa39903ed4fa60
Abdu Mohamdy <amohamdy@stanford.edu>
2020-08-25 13:28:23 -0700

    updated graph_benchmark

--- script/graph_benchmark.py
+++ script/graph_benchmark.py
@@ -23,31 +23,36 @@ def fill_params_map(files_map, run_files):
             run_params = tuple(list(literal_eval(inpt).values()))
         files_map[get_filename(file, '_Z3_', '.run')] = run_params
 
-def fill_kod_map(kod_results, result_files, params_map):
+def get_kod_params(filename):
+    so_far = filename[:filename.rfind('.kod.out')]
+    classname, ition, remove_index, check_index, bound = so_far.rsplit('_', 4)
+    return classname, ition, int(remove_index), int(check_index), int(bound)
+
+
+def fill_kod_map(kod_results, result_files):
     for file in result_files:
         print('file', file)
         with open(file, 'r') as f:
-            result = literal_eval(f.read())
-        filename = get_filename(file, '_KOD_', '.result')
-        basename = filename[:filename.rfind('_')]
-
-        if basename in params_map:
-            params = params_map[basename]
-        else:
-            print(f'File not found in params_map {file}')
-            continue
-        # classname = re.split('\d+', basename, 1)[0][:-1] # First part without the trailing '_'
-        classname = basename[:filename.rfind('_')]
-        kod_results[classname] = params
+            try:
+                result = literal_eval(f.read())
+            except:
+                continue
+        filename = os.path.basename(file)
+        classname, ition, remove_index, check_index, bound = get_kod_params(filename)
+        if not classname in kod_results:
+            kod_results[classname] = {}
+        if not (ition, remove_index, check_index) in kod_results[classname]:
+            kod_results[classname][(ition, remove_index, check_index)] = {}
+        kod_results[classname][(ition, remove_index, check_index)][bound] = (result['outcome'], result['solving_time'])
 
 def main():
-    kod_results_files = [os.path.join(root, f) for root, _, files in os.walk(KOD_RESULTS_DIRECTORY_PATH) for f in files if re.match(r'.*[.]result', f)]
-    z3_results_files = [os.path.join(root, f) for root, _, files in os.walk(Z3_RESULTS_DIRECTORY_PATH) for f in files if re.match(r'.*[.]z3[.]out', f)]
-    run_files = [os.path.join(root, f) for root, _, files in os.walk(Z3_RESULTS_DIRECTORY_PATH) for f in files if re.match(r'.*[.]run', f)]
-    params_map = {}
-    fill_params_map(params_map, run_files)
+    kod_results_files = [os.path.join(root, f) for root, _, files in os.walk(KOD_RESULTS_DIRECTORY_PATH) for f in files if re.match(r'.*[.]kod[.]out', f)]
+    # z3_results_files = [os.path.join(root, f) for root, _, files in os.walk(Z3_RESULTS_DIRECTORY_PATH) for f in files if re.match(r'.*[.]z3[.]out', f)]
+    # run_files = [os.path.join(root, f) for root, _, files in os.walk(Z3_RESULTS_DIRECTORY_PATH) for f in files if re.match(r'.*[.]run', f)]
+    # params_map = {}
+    # fill_params_map(params_map, run_files)
     kod_results = {}
-    fill_kod_map(kod_results, kod_results_files, params_map)
+    fill_kod_map(kod_results, kod_results_files)
     print('kod_results:', kod_results)
 
     # print('map: ', files_map)
